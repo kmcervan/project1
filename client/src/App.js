9cf761dcc1039cb2a6d52cb8f512198dc16d367c
karla-cervantes <cervantes.karla14@gmail.com>
2021-02-11 16:15:24 -0600

    saved

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        API Data: {data}       
+        Data for API: {data}       
       </header>
     </div>
   );
